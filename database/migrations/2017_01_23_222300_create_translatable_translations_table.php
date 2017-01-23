6d4d3ea2b40de592c03bcc65b66bdd8540e85246
Rathes Sachchithananthan <rathes@aheenam.com>
2017-01-23 23:20:46 +0100

    Fixes wrong convention name

--- database/migrations/2017_01_23_222300_create_translatable_translations_table.php
+++ database/migrations/2017_01_23_222300_create_translatable_translations_table.php
@@ -15,7 +15,7 @@ class CreateTranslatableTranslationsTable extends Migration
         Schema::create('translatable_translations', function ($table) {
 
             $table->increments('id');
-            $table->string('translatable_model');
+            $table->string('translatable_type');
             $table->integer('translatable_id');
             $table->string('key');
             $table->text('translation');
