35c5184bb65d08cb763402b32106cf86742ec745
Rathes Sachchithananthan <rathes@aheenam.com>
2017-01-23 23:27:58 +0100

    Fixes wrong convention name

--- database/migrations/2017_01_23_222300_create_translatable_translations_table.php
+++ database/migrations/2017_01_23_222300_create_translatable_translations_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateTranslatableTranslationsTable extends Migration
@@ -12,14 +13,14 @@ class CreateTranslatableTranslationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('translatable_translations', function ($table) {
+        Schema::create('translatable_translations', function (Blueprint $table) {
 
             $table->increments('id');
             $table->string('translatable_type');
             $table->integer('translatable_id');
             $table->string('key');
             $table->text('translation');
-            $table->text('language')->nullable();
+            $table->string('locale', 5);
             $table->timestamps();
 
             $table->index(['translatable_id', 'translatable_model']);
