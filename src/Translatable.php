c4d2fe41f70a3961656147cfbc2d34ed5408a056
Rathes Sachchithananthan <rathes@aheenam.com>
2017-01-23 22:22:02 +0100

    Adds package boilerplate

---
+++ src/Translatable.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aheenam\Translatable;
+
+trait Translatable {
+
+
+
+}
---
+++ src/TranslatableServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Aheenam\Translatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class TranslatableServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
