2e500c241467ccb985ca5998d5b2f8b6cee8c2ea
Rathes Sachchithananthan <rathes@aheenam.com>
2017-01-23 22:30:50 +0100

    Adds database structure

---
+++ database/migrations/2017_01_23_222300_create_translatable_translations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTranslatableTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translatable_translations', function ($table) {
+
+            $table->increments('id');
+            $table->string('translatable_model');
+            $table->integer('translatable_id');
+            $table->string('key');
+            $table->text('translation');
+            $table->text('language')->nullable();
+            $table->timestamps();
+
+            $table->index(['translatable_id', 'translatable_model']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('translatable_translations');
+    }
+}
